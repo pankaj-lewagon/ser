110389049f31b97d1b72c8d4a56ec56cc27bd184
Michael Michael <mmichael.esq@gmail.com>
2021-08-30 17:36:33 +0100

    basic function for extracting features from a file

---
+++ ser/extract_feature.py
@@ -0,0 +1,28 @@
+import librosa
+import soundfile
+import os, glob, pickle
+import numpy as np
+
+#DataFlair - Extract features (mfcc, chroma, mel) from a sound file
+def extract_feature(file_name, mfcc, chroma, mel):
+    with soundfile.SoundFile(file_name) as sound_file:
+        X = sound_file.read(dtype="float32")
+        sample_rate=sound_file.samplerate
+        if chroma:
+            stft=np.abs(librosa.stft(X))
+        result=np.array([])
+        if mfcc:
+            mfccs=np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
+            result=np.hstack((result, mfccs))
+        if chroma:
+            chroma=np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
+            result=np.hstack((result, chroma))
+        if mel:
+            mel=np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T,axis=0)
+            result=np.hstack((result, mel))
+    return result
+
+if __name__ == '__main__':
+    file_name = 'raw_data/ravdess_data/Actor_01/03-01-01-01-01-02-01.wav'
+    feature_array = extract_feature(file_name, True, True, True)
+    print(feature_array)
